989ae6e6d31fbdd5aaf68f721f3274de8582206b
dekuNukem <dekunukem@gmail.com>
2020-06-01 14:27:37 +0100

    push script fix

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
-find . -name "duckapp" -exec rm -rf {} \;
+find . -name "duck_app" -exec rm -rf {} \;
 zip -rv sample_profiles.zip sample_profiles
 git add --all
 git commit -m "$@"
