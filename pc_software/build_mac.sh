c225ed7d15cea2c01c6284818b9cca1611a2abb0
dekuNukem <dekunukem@gmail.com>
2020-06-01 14:26:50 +0100

    macos pyinstaller working

---
+++ pc_software/build_mac.sh
@@ -0,0 +1,9 @@
+rm -rfv ./duck_app
+mkdir ./duck_app
+cp ./*.py ./duck_app
+cp ./*.spec ./duck_app
+cp ./icon.ico ./duck_app
+cd ./duck_app
+
+# pyinstaller --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' --noconsole --icon=icon.ico main.py
+pyinstaller main_mac.spec
