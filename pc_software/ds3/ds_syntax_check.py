6ab9606a89c16b9281afe31bb6bbd5ea286f4e96
dekunukem <dekunukem@gmail.com>
2023-01-19 17:14:09 +0000

    started working on new configurator version with duckyscript3 support

---
+++ pc_software/ds3/ds_syntax_check.py
@@ -0,0 +1,254 @@
+import os
+
+autogui_map = {"ESCAPE":"escape",
+"ESC":"esc",
+"ENTER":"enter",
+"UP":"up",
+"DOWN":"down",
+"LEFT":"left",
+"RIGHT":"right",
+"UPARROW":"up",
+"DOWNARROW":"down",
+"LEFTARROW":"left",
+"RIGHTARROW":"right",
+"F1":"f1",
+"F2":"f2",
+"F3":"f3",
+"F4":"f4",
+"F5":"f5",
+"F6":"f6",
+"F7":"f7",
+"F8":"f8",
+"F9":"f9",
+"F10":"f10",
+"F11":"f11",
+"F12":"f12",
+"F13":"f13",
+"F14":"f14",
+"F15":"f15",
+"F16":"f16",
+"F17":"f17",
+"F18":"f18",
+"F19":"f19",
+"F20":"f20",
+"F21":"f21",
+"F22":"f22",
+"F23":"f23",
+"F24":"f24",
+"BACKSPACE":"backspace",
+"TAB":"tab",
+"CAPSLOCK":"capslock",
+"PRINTSCREEN":"printscreen",
+"SCROLLLOCK":"scrolllock",
+"PAUSE":"pause",
+"BREAK":"pause",
+"INSERT":"insert",
+"HOME":"home",
+"PAGEUP":"pageup",
+"PAGEDOWN":"pagedown",
+"DELETE":"delete",
+"END":"end",
+"SPACE":"space",
+
+"SHIFT":"shift",
+"RSHIFT":"shiftright",
+
+"ALT":"alt",
+"RALT":"altright",
+"OPTION":"option",
+"ROPTION":"optionright",
+
+"GUI":"win",
+"WINDOWS":"win",
+"COMMAND":"command",
+"RWINDOWS":"winright",
+"RCOMMAND":"winright",
+
+"CONTROL":"ctrl",
+"CTRL":"ctrl",
+"RCONTROL":"ctrlright",
+"RCTRL":"ctrlright",
+
+"MK_VOLUP":"volumeup",
+"MK_VOLDOWN":"volumedown",
+"MK_MUTE":"volumemute",
+"MK_PREV":"prevtrack",
+"MK_NEXT":"nexttrack",
+"MK_PP":"playpause",
+"MK_STOP":"stop",
+"MENU":"apps",
+"APP":"apps",
+"NUMLOCK":"numlock",
+"KP_SLASH":"/",
+"KP_ASTERISK":"*",
+"KP_MINUS":"-",
+"KP_PLUS":"+",
+"KP_ENTER":"enter",
+"KP_1":"num1",
+"KP_2":"num2",
+"KP_3":"num3",
+"KP_4":"num4",
+"KP_5":"num5",
+"KP_6":"num6",
+"KP_7":"num7",
+"KP_8":"num8",
+"KP_9":"num9",
+"KP_0":"num0",
+"KP_DOT":".",
+"KP_EQUAL":"=",
+"POWER":"",
+"LMOUSE":"",
+"RMOUSE":"",
+"MMOUSE":"",
+}
+
+valid_chars = ['!', '"', '#', '$', '%', '&', "'", '(',
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+
+cmd_REPEAT = "REPEAT "
+cmd_REM = "REM "
+cmd_DEFAULTDELAY = "DEFAULTDELAY "
+cmd_DEFAULTCHARDELAY = "DEFAULTCHARDELAY "
+cmd_DELAY = "DELAY "
+cmd_STRING = "STRING "
+cmd_STRINGLN = "STRINGLN "
+
+cmd_EMUK = "EMUK "
+cmd_LOOP = "LOOP"
+cmd_SWCOLOR = "SWCOLOR_";
+cmd_SW_SELF_COLOR = "SWCOLOR ";
+
+cmd_LMOUSE = "LMOUSE"
+cmd_RMOUSE = "RMOUSE"
+cmd_MMOUSE = "MMOUSE"
+cmd_MOUSE_MOVE = "MOUSE_MOVE"
+cmd_MOUSE_WHEEL = "MOUSE_WHEEL"
+cmd_KEYDOWN = "KEYDOWN ";
+cmd_KEYUP = "KEYUP ";
+
+cmd_CHARJITTER = "CHARJITTER ";
+
+mouse_commands = [cmd_LMOUSE, cmd_RMOUSE, cmd_MMOUSE, cmd_MOUSE_MOVE, cmd_MOUSE_WHEEL]
+
+ignored_but_valid_commands = ["UARTPRINT ", "LCR", cmd_CHARJITTER, cmd_REM, "SWCOLOR_", "SWCOLOR ", 'DP_SLEEP', 'PREV_PROFILE', 'NEXT_PROFILE', 'GOTO_PROFILE ', 'INJECT_MOD', "KATAKANAHIRAGANA", "HENKAN", "MUHENKAN", "KATAKANA", "HIRAGANA", "ZENKAKUHANKAKU"]
+def is_ignored_but_valid_command(ducky_line):
+	for item in ignored_but_valid_commands:
+		if ducky_line.startswith(item):
+			return True
+	return False
+
+PARSE_OK = 0
+PARSE_ERROR = 1
+
+def parse_combo(combo_line):
+	combo_keys = [x for x in combo_line.split(' ') if len(x) > 0]
+	if len(combo_keys) > 6:
+		return PARSE_ERROR, 'No more than 6 combos'
+	for item in [x.lower() for x in combo_keys if x not in autogui_map.keys()]:
+		if item not in valid_chars:
+			return PARSE_ERROR, 'Invalid combo character'
+	return PARSE_OK, 'Success'
+
+def parse_mouse(ducky_line):
+	mouse_command_list = [x for x in mouse_commands if x in ducky_line]
+	if len(mouse_command_list) != 1:
+		return PARSE_ERROR, 'Mouse command too many arguments'
+	this_mouse_command = mouse_command_list[0]
+	if this_mouse_command == cmd_LMOUSE:
+		return PARSE_OK, "Success"
+	elif this_mouse_command == cmd_RMOUSE:
+		return PARSE_OK, "Success"
+	elif this_mouse_command == cmd_MMOUSE:
+		return PARSE_OK, "Success"
+	elif this_mouse_command == cmd_MOUSE_MOVE:
+		try:
+			x_amount = int([x for x in ducky_line.split(' ') if len(x) > 0][1])
+			y_amount = int([x for x in ducky_line.split(' ') if len(x) > 0][2])
+			if x_amount > 127 or x_amount < -127:
+				raise ValueError
+			if y_amount > 127 or y_amount < -127:
+				raise ValueError
+		except:
+			return PARSE_ERROR, 'MOUSE_MOVE value error: should be -127 to 127'
+		return PARSE_OK, "Success"
+	elif this_mouse_command == cmd_MOUSE_WHEEL:
+		try:
+			amount = int(ducky_line.split(' ')[1])
+			if amount > 127 or amount < -127:
+				raise ValueError
+		except:
+			return PARSE_ERROR, 'MOUSE_WHEEL value error: should be -127 to 127'
+		return PARSE_OK, "Success"
+	return PARSE_ERROR, "Invalid mouse command"
+
+def parse_line(ducky_line):
+	parse_result = PARSE_OK
+	parse_message = 'Unknown'
+	ducky_line = ducky_line.replace('\n', '').replace('\r', '')
+	if not (ducky_line.startswith(cmd_STRING) or ducky_line.startswith(cmd_STRINGLN) or ducky_line.startswith(cmd_REM)):
+		ducky_line = ducky_line.strip()
+	if len(ducky_line) == 0:
+		return PARSE_OK, "Empty line"
+	elif len(ducky_line) >= 250:
+		return PARSE_ERROR, "Line too long, max 250 characters per line."
+	elif is_ignored_but_valid_command(ducky_line):
+		return PARSE_OK, "Success"
+	elif ducky_line.startswith(cmd_KEYDOWN):
+		parse_result, parse_message = parse_combo(ducky_line[len(cmd_KEYDOWN):])
+	elif ducky_line.startswith(cmd_KEYUP):
+		parse_result, parse_message = parse_combo(ducky_line[len(cmd_KEYUP):])
+	elif ducky_line.startswith(cmd_LOOP) and ducky_line.endswith(':') and len(ducky_line) == 6 and ducky_line[4].isnumeric():
+		return PARSE_OK, "Success"
+	elif ducky_line.startswith(cmd_EMUK):
+		sssss = ducky_line[len(cmd_EMUK):].strip().split(' ')
+		if len(sssss) > 2:
+			return PARSE_ERROR, "EMUK max 2 keys"
+		if sssss[0] in autogui_map.keys() or sssss[0] in mouse_commands[:3]:
+			return PARSE_OK, "Success"
+		elif len(sssss[0]) == 1:
+			return PARSE_OK, "Success"
+		return PARSE_ERROR, "EMUK invalid key"
+	elif ducky_line.startswith(cmd_STRING) or ducky_line.startswith(cmd_STRINGLN):
+		return PARSE_OK, "Success"
+	elif ducky_line.startswith(cmd_REPEAT):
+		try:
+			int(ducky_line[len(cmd_REPEAT):].strip())
+		except Exception:
+			return PARSE_ERROR, "REPEAT invalid argument"
+		return PARSE_OK, "Success"
+	elif ducky_line.startswith(cmd_DELAY):
+		try:
+			if int(ducky_line[len(cmd_DELAY):].strip())/1000 <= 0:
+				return PARSE_ERROR, "DELAY can't be zero or negative"
+			else:
+				return PARSE_OK, "Success"
+		except Exception:
+			return PARSE_ERROR, "DELAY invalid argument"
+	elif ducky_line.startswith(cmd_DEFAULTDELAY):
+		try:
+			if int(ducky_line[len(cmd_DEFAULTDELAY):].strip()) <= 0:
+				return PARSE_ERROR, "DEFAULTDELAY can't be zero or negative"
+			else:
+				return PARSE_OK, "Success"
+		except Exception:
+			return PARSE_ERROR, "DEFAULTDELAY invalid argument"
+	elif ducky_line.startswith(cmd_DEFAULTCHARDELAY):
+		try:
+			if int(ducky_line[len(cmd_DEFAULTCHARDELAY):].strip()) <= 0:
+				return PARSE_ERROR, "DEFAULTCHARDELAY can't be zero or negative"
+			else:
+				return PARSE_OK, "Success"
+		except Exception:
+			return PARSE_ERROR, "DEFAULTCHARDELAY invalid argument"
+	elif ducky_line.split(' ')[0] in autogui_map.keys():
+		parse_result, parse_message = parse_combo(ducky_line)
+	elif ducky_line.split(' ')[0] in mouse_commands:
+		parse_result, parse_message = parse_mouse(ducky_line)
+	else:
+		parse_result = PARSE_ERROR
+		parse_message = "Invalid command"
+	return parse_result, parse_message
+
---
+++ pc_software/ds3/duck_objs.py
@@ -0,0 +1,252 @@
+import os
+import sys
+import check_update
+import json
+import socket
+import urllib.request
+
+MAX_PROFILE_COUNT = 32
+
+class dp_key(object):
+	def load_script(self, path):
+		try:
+			with open(path, encoding='utf8') as keyfile:
+				return keyfile.read()
+		except Exception as e:
+			print('load_script exception:', e)
+			return ""
+
+	def read_color(self, config_path):
+		with open(config_path) as configfile:
+			for line in configfile:
+				line = line.replace('\n', '').replace('\r', '')
+				hotword = "SWCOLOR_" + str(self.index) + ' '
+				while('  ' in line):
+					line = line.replace('  ', ' ')
+				if hotword in line:
+					temp_split = line.split(' ')
+					self.color = (int(temp_split[1]), int(temp_split[2]), int(temp_split[3]))
+
+	def __str__(self):
+		ret = ""
+		ret += str('...............') + '\n'
+		ret += "path:\t" + str(self.path) + '\n'
+		ret += "name:\t" + str(self.name) + '\n'
+		ret += "index:\t" + str(self.index) + '\n'
+		ret += "color:\t" + str(self.color) + '\n'
+		ret += "script:\t" + str(len(self.script)) + " characters\n"
+		ret += str('...............') + '\n'
+		return ret
+
+	def get_keyname(self, path, index):
+		ret = 'ERR' + str(index)
+		try:
+			with open(os.path.join(os.path.dirname(path), "config.txt")) as ffffff:
+				for line in ffffff:
+					this_split = line.replace('\n','').replace('\r','').split(' ', 1)
+					if this_split[0].startswith('z') and int(this_split[0][1:]) == index:
+						ret = this_split[1]
+						break
+		except Exception as e:
+			print('get_keyname exception:', e)
+			pass
+		return ret
+
+	def is_loop(self, line):
+		try:
+			if line.startswith("LOOP") and line.endswith(':') and len(line) == 6 and line[4].isnumeric():
+				return True, int(line[4])
+		except Exception as e:
+			print('is_loop', e)
+			# pass
+		return False, 0
+
+	def check_loop(self):
+		for line in self.script.split('\n'):
+			line = line.replace('\n', '').replace('\r', '')
+			aaa, bbb = self.is_loop(line)
+			if aaa:
+				self.has_loop = True
+			if bbb > self.max_loop:
+				self.max_loop = bbb
+		
+	def __init__(self, path=None):
+		super(dp_key, self).__init__()
+		self.path = path
+		self.name = None
+		self.index = None
+		self.color = None
+		self.has_loop = False
+		self.max_loop = 0
+		self.script = ''
+		if path is None:
+			return
+		self.index = int(os.path.basename(os.path.normpath(path)).split("_")[0].split(".txt")[0].strip('key'))
+		if '_' in os.path.basename(os.path.normpath(path)):
+			self.name = os.path.basename(os.path.normpath(path)).rsplit('.', 1)[0].split('_', 1)[-1]
+		else:
+			self.name = self.get_keyname(path, self.index)
+		self.color = None
+		self.script = self.load_script(path)
+		try:
+			config_path = os.path.join(os.path.dirname(path), "config.txt")
+			self.read_color(config_path)
+		except Exception as e:
+			print(">>> read_color:", config_path, e)
+			pass
+
+# -----------------------------------------------------------
+
+class dp_profile(object):
+	def read_keys(self, path):
+		key_file_list = [x for x in os.listdir(path) if x.endswith('.txt') and x.startswith('key') and x[3].isnumeric()]
+		key_file_list.sort(key=lambda s: int(s[3:].split("_")[0].split(".txt")[0])) # sort by number not by letter
+		# print(key_file_list)
+		for item in key_file_list:
+			this_key = dp_key(os.path.join(path, item))
+			self.keylist[this_key.index - 1] = this_key
+
+	def read_config(self, path):
+		try:
+			with open(os.path.join(path, "config.txt")) as configfile:
+				for line in configfile:
+					line = line.replace('\n', '').replace('\r', '')
+					while('  ' in line):
+						line = line.replace('  ', ' ')
+					if line.startswith('BG_COLOR '):
+						temp_split = line.split(' ')
+						self.bg_color = (int(temp_split[1]), int(temp_split[2]), int(temp_split[3]))
+					if line.startswith('KEYDOWN_COLOR '):
+						temp_split = line.split(' ')
+						self.kd_color = (int(temp_split[1]), int(temp_split[2]), int(temp_split[3]))
+					if line.startswith("DIM_UNUSED_KEYS 0"):
+						self.dim_unused = False
+		except Exception as e:
+			print('>>>>> read_config:', path, e)
+			pass
+
+	def load_from_path(self, path):
+		folder_name = os.path.basename(os.path.normpath(path))
+		if not (folder_name.startswith('profile') and folder_name[7].isnumeric() and '_' in folder_name):
+			print("invalid profile folder:", folder_name)
+			return
+		self.path = path
+		self.name = folder_name.split('_', 1)[-1]
+		self.read_config(path)
+		self.read_keys(path)
+
+	def __str__(self):
+		ret = ""
+		ret += str('-----Profile info-----') + '\n'
+		ret += "path:\t" + str(self.path) + '\n'
+		ret += "name:\t" + str(self.name) + '\n'
+		ret += "bg_color:\t" + str(self.bg_color) + '\n'
+		ret += "kd_color:\t" + str(self.kd_color) + '\n'
+		ret += "dim_unused:\t" + str(self.dim_unused) + '\n'
+		ret += "key count:\t" + str(len([x for x in self.keylist if x is not None])) + '\n'
+		ret += "keys:\n"
+		for item in [x for x in self.keylist]:
+			ret += str(item) + '\n'
+		ret += str('----------------------') + '\n'
+		return ret
+
+	def __init__(self):
+		super(dp_profile, self).__init__()
+		self.key_count = 15
+		self.path = None
+		self.name = None
+		self.keylist = [None] * self.key_count
+		self.bg_color = (84,22,180)
+		self.kd_color = None
+		self.dim_unused = True
+
+class dp_global_settings(object):
+	def load_from_path(self, path):
+		try:
+			with open(os.path.join(path, 'dp_settings.txt')) as settings_file:
+				file_lines = settings_file.readlines()
+				self.list_of_lines = file_lines
+				for line in file_lines:
+					line = line.replace('\n', '').replace('\r', '')
+					if 'sleep_after_min' in line:
+						self.sleep_after_minutes = int(line.split(' ')[-1])
+		except Exception as e:
+			print('dp_global_settings load_from_path:', e)
+	def __init__(self):
+		super(dp_global_settings, self).__init__()
+		self.sleep_after_minutes = 30
+		self.list_of_lines = []
+
+class dp_keymap(object):
+	def __init__(self):
+		super(dp_keymap, self).__init__()
+		self.file_name = ''
+		self.display_name = ''
+		self.is_valid = 0
+		self.content = ''
+		self.url = None
+
+	def __str__(self):
+		ret = ""
+		ret += str(self.display_name) + '\n'
+		ret += str(self.file_name) + '\n'
+		ret += str(self.url) + '\n'
+		ret += str(self.is_valid) + '\n'
+		ret += str(self.content[:10]) + '\n'
+		ret += '------------'
+		return ret
+
+def build_profile(root_dir_path):
+	my_dirs = [d for d in os.listdir(root_dir_path) if os.path.isdir(os.path.join(root_dir_path, d))]
+	my_dirs = [x for x in my_dirs if x.startswith('profile') and x[7].isnumeric() and '_' in x]
+	my_dirs.sort(key=lambda s: int(s[7:].split("_")[0]))
+	my_dirs = [os.path.join(root_dir_path, d) for d in my_dirs if d.startswith("profile")]
+	my_dirs = my_dirs[:MAX_PROFILE_COUNT]
+	profile_list = []
+	for item in my_dirs:
+		this_profile = dp_profile()
+		this_profile.load_from_path(item)
+		profile_list.append(this_profile)
+	return profile_list
+
+def load_keymap_from_file(file_path):
+	this_keymap = dp_keymap()
+	this_keymap.file_name = os.path.basename(file_path)
+	this_keymap.display_name = this_keymap.file_name.replace('dpkm_', '').replace('.txt', '')
+	with open(file_path, encoding='utf8') as keymap_file:
+		this_keymap.content = keymap_file.readlines()
+	this_keymap.is_valid = 1
+	return this_keymap
+
+def load_keymap(root_dir_path):
+	keymap_folder_path = os.path.join(root_dir_path, 'keymaps')
+	if os.path.isdir(keymap_folder_path) is False:
+		return []
+	keymap_file_list = [d for d in os.listdir(keymap_folder_path) if d.startswith("dpkm_") and d.endswith(".txt")]
+	return_list = []
+	for item in keymap_file_list:
+		this_keymap = load_keymap_from_file(os.path.join(keymap_folder_path, item))
+		if this_keymap.is_valid:
+			return_list.append(this_keymap)
+	return return_list
+
+keymap_url = 'https://api.github.com/repos/dekuNukem/duckyPad/contents/sample_profiles/keymaps?ref=master'
+
+def load_online_keymap():
+	if check_update.is_internet_available() is False:
+		return []
+	file_list = json.loads(urllib.request.urlopen(keymap_url).read())
+	return_list = []
+	for item in file_list:
+		if not ('name' in item and 'type' in item and item['type'] == 'file'):
+			continue
+		if not (item['name'].startswith("dpkm_") and item['name'].endswith(".txt")):
+			continue
+		this_keymap = dp_keymap()
+		this_keymap.file_name = item['name']
+		this_keymap.display_name = this_keymap.file_name.replace('dpkm_', '').replace('.txt', '')
+		# this_keymap.content = str(urllib.request.urlopen(item['download_url']).read().decode('utf-8')).split('\n')
+		this_keymap.url = item['download_url'] # download the content later, when saving
+		this_keymap.is_valid = 1
+		return_list.append(this_keymap)
+	return return_list
