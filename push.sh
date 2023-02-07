f31d157996ae9329ff4b8e8b3146b580616bd8fb
dekunukem <dekunukem@gmail.com>
2023-02-07 20:45:58 +0000

    cleaned up bytecode compiler output

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 find . -type f -name "*.b#*" -exec rm -f {} \;
+find . -type f -name "*.dsb" -exec rm -f {} \;
 find . -type f -name "*.s#*" -exec rm -f {} \;
 find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
