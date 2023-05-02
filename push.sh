4ceff93764dbdbb577978d282ec321808b56e034
dekuNukem <dekunukem@gmail.com>
2023-05-03 00:47:20 +0100

    prepare for mac build

--- push.sh
+++ push.sh
@@ -9,6 +9,7 @@ find . -type f -name "*.s#*" -exec rm -f {} \;
 find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
+find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 find ./pc_software -name "build" -exec rm -rf {} \;
 find ./pc_software -name "dist" -exec rm -rf {} \;
