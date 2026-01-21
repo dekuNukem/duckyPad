aaa11c2830ca9e481d572892939229a54a72f3bf
dekuNukem <dekunukem@gmail.com>
2026-01-21 15:02:48 +0000

    test

--- push.sh
+++ push.sh
@@ -1,19 +1,29 @@
-if [[ $# -eq 0 ]] ; then
-    echo 'error: please enter a commit comment'
-    exit 1
+#!/bin/bash
+
+# If no arguments are provided, ask for a comment
+if [[ $# -eq 0 ]]; then
+    read -p "Enter a commit message: " commit_msg
+    if [[ -z "$commit_msg" ]]; then
+        echo "error: commit message cannot be empty"
+        exit 1
+    fi
+else
+    commit_msg="$*"
 fi
 
-find . -type f -name "*.b#*" -exec rm -f {} \;
-# find . -type f -name "*.dsb" -exec rm -f {} \;
-find . -type f -name "*.s#*" -exec rm -f {} \;
-find . -type f -name "*.l#*" -exec rm -f {} \;
-find . -type f -name "*.csv#*" -exec rm -f {} \;
+# Cleanup temp and cache files
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
-find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
-rm -rfv ./resources/new_hid_dump/hid_dump
+find . -type f -name "main.exe" -exec rm -f {} \;
+find . -type f -name "main" -exec rm -f {} \;
+find . -type f -name "*.dsb" -exec rm -f {} \;
+find . -type f -name "pgmlst*" -exec rm -f {} \;
 
+# Get current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Stage, commit, and push
 git add --all
-git commit -m "$@"
-git push origin master
+git commit -m "$commit_msg"
+git push origin "$current_branch"
